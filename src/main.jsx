870a5fa7e09deb1e35a6a2ad5bdcaa01e7d79fce
eoinoliver <eoinoliver@gmail.com>
2026-04-23 16:13:29 +1000

    Add Google Analytics and Microsoft Clarity tracking

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './src/App.jsx'
 
+// Google Analytics
+const GA_ID = 'G-6CXYVZMWYF'
+const gaScript = document.createElement('script')
+gaScript.async = true
+gaScript.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`
+document.head.appendChild(gaScript)
+window.dataLayer = window.dataLayer || []
+function gtag() { window.dataLayer.push(arguments) }
+window.gtag = gtag
+gtag('js', new Date())
+gtag('config', GA_ID)
+
+// Microsoft Clarity
+;(function(c, l, a, r, i, t, y) {
+    c[a] = c[a] || function() { (c[a].q = c[a].q || []).push(arguments) }
+    t = l.createElement(r); t.async = 1; t.src = 'https://www.clarity.ms/tag/' + i
+    y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y)
+})(window, document, 'clarity', 'script', 'wg1sd0nuox')
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><App /></React.StrictMode>,
-)
+    <React.StrictMode><App /></React.StrictMode>React.StrictMode>,
+  )</React.StrictMode>
