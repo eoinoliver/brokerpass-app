64f261e8ae3ab2c0ad381d536579142cb78a9047
eoinoliver <eoinoliver@gmail.com>
2026-04-23 16:22:47 +1000

    Fix: simplify GA and Clarity tracking initialization

--- src/main.jsx
+++ src/main.jsx
@@ -3,24 +3,26 @@ import ReactDOM from 'react-dom/client'
 import App from './src/App.jsx'
 
 // Google Analytics
-const GA_ID = 'G-6CXYVZMWYF'
-const gaScript = document.createElement('script')
-gaScript.async = true
-gaScript.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`
-document.head.appendChild(gaScript)
 window.dataLayer = window.dataLayer || []
 function gtag() { window.dataLayer.push(arguments) }
 window.gtag = gtag
 gtag('js', new Date())
-gtag('config', GA_ID)
+gtag('config', 'G-6CXYVZMWYF')
+const gaEl = document.createElement('script')
+gaEl.async = true
+gaEl.src = 'https://www.googletagmanager.com/gtag/js?id=G-6CXYVZMWYF'
+document.head.appendChild(gaEl)
 
 // Microsoft Clarity
-;(function(c, l, a, r, i, t, y) {
-    c[a] = c[a] || function() { (c[a].q = c[a].q || []).push(arguments) }
-    t = l.createElement(r); t.async = 1; t.src = 'https://www.clarity.ms/tag/' + i
-    y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y)
-})(window, document, 'clarity', 'script', 'wg1sd0nuox')
+window.clarity = window.clarity || function() {
+      (window.clarity.q = window.clarity.q || []).push(arguments)
+}
+const clEl = document.createElement('script')
+clEl.async = true
+clEl.src = 'https://www.clarity.ms/tag/wg1sd0nuox'
+const firstScript = document.getElementsByTagName('script')[0]
+firstScript.parentNode.insertBefore(clEl, firstScript)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode><App /></React.StrictMode>React.StrictMode>,
-  )</React.StrictMode>
+      <React.StrictMode><App /></React.StrictMode>React.StrictMode>,
+    )</React.StrictMode>
